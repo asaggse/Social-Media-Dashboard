555cfdaeee1162542d96ee6a7270ece816328fa9
Alessandro Saggese <asaggese@gmail.com>
2023-06-06 12:36:30 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,47 @@ sass.compiler = require('dart-sass');
 // Sass Task
 function scssTask() {
     return src('app/scss/style.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest('dist', { sourcemaps: '.'}))
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('dist', { sourcemaps: '.' }))
 }
 
 // JavaScript Task
 function jsTask() {
-    return src('app/js/script.js', { sourcemaps: true})
-    .pipe(babel({ presets: ['@babel/preset-env'] }))
-    .pipe(terser())
-    .pipe(dest('dist', { sourcemaps: '.' }));
+    return src('app/js/script.js', { sourcemaps: true })
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.' }));
 }
+
+// Browsersync
+function browserSyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+function browserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browsersyncReload);
+    watch(
+        ['app/scss/**/*.scss', 'app/**/*.js'],
+        series(scssTask, jsTask, browsersyncReload)
+    );
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
